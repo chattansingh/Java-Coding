12cd3d238128cd4b695ebc51a317f4f575d2e618
chattan <singhchattanthe@gmail.com>
2019-02-22 20:05:37 -0800

    Add files via upload

---
+++ QuickSort.java
@@ -0,0 +1,45 @@
+
+
+
+package Sort;
+
+	public class QuickSort {
+		public static void main(String[] args) {
+			int[] n = {1,3,7,9,2,4,6,0,11,66,44,99,765,999,-1,55};
+//			QuickSort qs = new QuickSort();
+//			qs.sort(n,0,n.length-1);
+			new QuickSort().quickSort(n,0,n.length-1);
+			for(int i = 0; i < n.length; i++)
+				System.out.print(n[i]+" ");
+		}
+		//s means start, e means end, st means stand number
+		void quickSort(int[] a, int s, int e){
+			if(s < e){
+				int l = s, r = e;
+				int st = a[s];
+				while(l < r){
+					while(r > l){
+						if(a[r] < st){
+							a[l] = a[r];
+							l++;
+							break;
+						} else
+							r--;
+					}
+					while(l < r){
+						if(a[l] > st){
+							a[r] = a[l];
+							r--;
+							break;
+						} else
+							l++;
+					}
+					
+				}
+				a[l] = st;
+				quickSort(a, s, l-1);
+				quickSort(a, l+1, e);
+			}
+		}
+	
+	}
